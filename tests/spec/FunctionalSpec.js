b91ecdf19d6854faa4d2220a8f3a68011e43ea03
Juan Alvarez <juan.alsu@gmail.com>
2016-05-04 20:12:52 -0500

    Initial commit

---
+++ tests/spec/FunctionalSpec.js
@@ -0,0 +1,64 @@
+function sum(a, b) {
+    return a + b;
+}
+function times2(x) {
+    return x * 2;
+}
+var curriedSum = sewr.curry(sum);
+describe("sewrLib", function() {
+  it("should uncurry and return 3", function () {
+    var oldSumAgain = sewr.unCurry(curriedSum);
+    var r = oldSumAgain(1,2);
+    expect(r).toEqual(3);
+  });
+  it("should eagerly resolve, run curried function,  and return 12", function() {
+    var gimme5 = sewr.setup(5);
+    var r = gimme5.compose(curriedSum(1)).stitch(times2).applyAll();
+    expect(r).toEqual(12);
+  });
+  it("should use lazy one, run curried function, then return 20", function() {
+    var c = sewr.stitch(curriedSum(7)).stitch(times2);
+    expect(c.applyAll(3)).toEqual(20);
+  });
+  it("should result be deferred, pass in parameters and return 10", function() {
+    var lazied = sewr.stitch(curriedSum).stitch(times2);
+    var r = lazied.applyAll(1).applyAll(4);
+    expect(r).toEqual(10);
+  });
+  it("should lazy compose and return 20", function() {
+    var lazied = sewr.stitch(curriedSum(7)).stitch(times2).applyAll;
+    var c = sewr.setup(3).compose(lazied);
+    expect(c.applyAll()).toEqual(20);
+  });
+  it("should bind using lazyBinder one and return 20", function() {
+    var lazied = sewr.stitch(curriedSum).stitch(times2);
+    var r = sewr.lazyBinder(lazied)(3).applyAll(7);
+    expect(r).toEqual(20);
+  });
+  it("should bind curried and return 6", function() {
+    function identity(x) {
+      return 1 * x;
+    }
+    function lazied(x, y) {
+       return sewr.stitch(curriedSum).stitch(times2).applyAll(x).stitch(identity).applyAll(y);
+    }
+    var r = lazied(1, 2);
+    expect(r).toEqual(6);
+  });  
+  it("should compose curried and return 12", function() {
+    function customBind(x , y) {
+        var f = sewr.setup(x).stitch(curriedSum).stitch(times2).applyAll;
+        return f(y);
+    }
+    var r = customBind(3, 3);
+    expect(r).toEqual(12);
+  });
+  it("should compose curried, default to lazy and return 4", function() {
+    function customBind(x , y) {
+        var f = sewr.setup().stitch(curriedSum).stitch(times2).applyAll(x).applyAll;
+        return f(y);
+    }
+    var r = customBind(1, 1);
+    expect(r).toEqual(4);
+  });
+});
---
+++ tests/src/sewr.js
@@ -0,0 +1,110 @@
+//Author: Juan Alvarez
+//Version: 1.1.0
+'use strict';
+var sewr = (function functionalise () {
+    function createLazyObj(stack) {
+        var r = Object.create(null);
+        r.stitch = function (gamma) {
+            stack.push(gamma);
+            return createLazyObj(stack);
+        };
+        r.applyAll = function (x) {
+            var i;
+            var v = x;
+            var isCurried = false;
+            var restack = [];
+            for (i = 0; i < stack.length; i++) {
+                if (isCurried) {
+                    restack.push(stack[i]);
+                }
+                else {
+                    v = stack[i].call(null, v);    
+                    if (typeof v === 'function') {
+                        isCurried = true;
+                        restack.push(v);
+                    }
+                }
+            }
+            if (isCurried) {                       
+                return createLazyObj(restack);
+            }
+            else {
+                return v;
+            }
+        };
+        return r;
+    }
+    function createBindObj(current, original) {
+        var r = Object.create(null);
+        r.applyAll = function () {
+            return current;
+        };
+        r.stitch = function (gamma) {
+            var v;
+            if (current == null) {
+                v = gamma.call(null, original);
+            }
+            else {
+                v = gamma.call(null, current);
+            }
+            if (typeof v === 'function') {
+                return createLazyObj([v]);
+            }
+            if (typeof v === 'undefined') {
+                v = current;
+            }
+            return createBindObj(v, original);
+        };
+        return r;
+    }
+    var o = Object.create(null);
+    o.curry = function (gamma) {
+        return function (x) {
+            var list = [];
+            list.push(x);
+            function feedList(element) {
+                list.push(element);
+            }
+            return function () {
+                var args = Array.prototype.slice.call(arguments);
+                args.forEach(feedList);
+                return gamma.apply(null, list);
+            }    
+        }
+    };
+    o.unCurry = function (curriedGamma) {
+        return function () {
+            var args = Array.prototype.slice.call(arguments);
+            if (args.length == 0) {
+                return curriedGamma.call(null);
+            }
+            if (args.length == 1) {
+                return curriedGamma.call(null, args[0]); 
+            }
+            return curriedGamma.call(null, args[0]).apply(null, args.slice(1));
+        };
+    };
+    o.stitch = function (gamma) {
+        return createLazyObj([]).stitch(gamma);
+    };
+    o.lazyBinder = function (lazyObj) {
+        var f = lazyObj.applyAll;
+        return function (x) {
+            return f.call(null, x);
+        };
+    };
+    o.setup = function (value) {   
+        var m;
+        if (value == null) {
+            m = createLazyObj([]);
+        }    
+        else {
+            m = createBindObj(value, value);
+        } 
+        m.compose = function (beta) {
+            return m.stitch(beta);
+        };
+        return m;
+    };
+    return o;
+})(sewr);
